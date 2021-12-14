ccfce081ab566a80640212986b8fc010424aab03
LuigiMenez <ltc.menez@gmail.com>
2021-12-14 20:31:57 +0100

    Creation of the Map folder, with the index.jsx, and the style.jsx, in order to be able to display the map on the home page.

---
+++ src/Components/Map/style.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const SMap = styled.div`
+  /* width: 100%;
+  height: 100%; */
+  .leaflet-container {
+    height: 100vh;
+    width: 100vw;
+  }
+`;
+
+export default SMap;
