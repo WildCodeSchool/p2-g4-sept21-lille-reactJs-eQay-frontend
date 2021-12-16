2225218ef995271f2eb89dcdf65a69796e20b248
LuigiMenez <ltc.menez@gmail.com>
2021-12-16 16:55:50 +0100

    Added a scrollWheelZoom and to take off zoomControl panel

--- src/Components/Map/index.jsx
+++ src/Components/Map/index.jsx
@@ -4,7 +4,12 @@ import SMap from './style';
 export default function Map() {
   return (
     <SMap>
-      <MapContainer center={[50.629, 3.057]} zoom={13} scrollWheelZoom={false}>
+      <MapContainer
+        center={[50.629, 3.057]}
+        zoom={13}
+        scrollWheelZoom
+        zoomControl={false}
+      >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
