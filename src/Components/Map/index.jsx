9d140cb1209a1643218b031bbb0a25c3d1950c58
Pierre Goemans <p.goemans@hotmail.fr>
2021-12-21 10:41:18 +0100

    link backend - front end with more data

--- src/Components/Map/index.jsx
+++ src/Components/Map/index.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-
+import MarkerClusterGroup from 'react-leaflet-markercluster';
 import SMap from './style';
 
 export default function Map() {
@@ -9,16 +9,13 @@ export default function Map() {
 
   useEffect(() => {
     try {
-      console.log('coucou');
       axios.get('http://localhost:5050/indoor').then(({ data }) => {
-        setAllDataIndoors(data[0]);
+        setAllDataIndoors(data);
       });
     } catch (error) {
       console.log(error);
     }
   }, []);
-
-  console.log(allDataIndoors);
 
   return (
     <SMap>
@@ -32,15 +29,30 @@ export default function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {allDataIndoors.map((e) => {
-          return (
-            <Marker position={[e.adresses_latitude, e.adresses_longitude]}>
-              <Popup>
-                Vous êtes ici. <br /> Bienvenue à Lille.
-              </Popup>
-            </Marker>
-          );
-        })}
+        <MarkerClusterGroup>
+          {allDataIndoors.map((e) => {
+            return (
+              <Marker
+                position={[e[0].adresses_latitude, e[0].adresses_longitude]}
+              >
+                <Popup>
+                  <ul>
+                    <li>Aqi : {e[0].aqi}</li>
+                    <li>Pm1 : {e[0].pm1}</li>
+                    <li>Pm10 : {e[0].pm10}</li>
+                    <li>Pm25 : {e[0].pm25}</li>
+                    <li>Ppm : {e[0].ppm}</li>
+                    <li>Temperature : {e[0].temperature}</li>
+                    <li>Timestamp : {e[0].timestamp}</li>
+                    <li>Humidity : {e[0].humidity}</li>
+                    <li>lat : {e[0].adresses_latitude}</li>
+                    <li>long : {e[0].adresses_longitude}</li>
+                  </ul>
+                </Popup>
+              </Marker>
+            );
+          })}
+        </MarkerClusterGroup>
       </MapContainer>
     </SMap>
   );
