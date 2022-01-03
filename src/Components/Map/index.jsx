d5f49e94a5df71aace9d9718a538b1d46a63bceb
Pierre Goemans <p.goemans@hotmail.fr>
2022-01-03 16:29:36 +0100

    get pos of User

--- src/Components/Map/index.jsx
+++ src/Components/Map/index.jsx
@@ -1,27 +1,27 @@
-
-import {
-  MapContainer,
-  TileLayer,
-  Marker,
-  Popup,
-  useMapEvents,
-} from 'react-leaflet';
-import { useState } from 'react';
-
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
+import { useEffect, useState } from 'react';
 import SMap from './style';
 
 export default function Map() {
-  const [position, setPosition] = useState(null);
+  const [findUser, setFindUser] = useState(false);
+
+  function handleLocate() {
+    return setFindUser(!findUser);
+  }
+
   function LocationMarker() {
-    const map = useMapEvents({
-      click() {
-        map.locate();
-      },
-      locationfound(e) {
-        setPosition(e.latlng);
-        map.flyTo(e.latlng, map.getZoom());
-      },
-    });
+    const map = useMap();
+    const [position, setPosition] = useState(null);
+
+    useEffect(() => {
+      if (findUser) {
+        map.locate().on('locationfound', function (e) {
+          setPosition(e.latlng);
+          map.flyTo(e.latlng, map.getZoom());
+        });
+        setFindUser(false);
+      }
+    }, [position]);
 
     return position === null ? null : (
       <Marker position={position}>
@@ -29,27 +29,26 @@ export default function Map() {
       </Marker>
     );
   }
-
-
   return (
-    <SMap>
-      <MapContainer
-        center={[50.629, 3.057]}
-        zoom={13}
-        scrollWheelZoom
-        zoomControl={false}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[50.629, 3.057]}>
-          <Popup>
-            Vous êtes ici. <br /> Bienvenue à Lille.
-          </Popup>
-        </Marker>
-        <LocationMarker />
-      </MapContainer>
-    </SMap>
+    <>
+      <button type="button" onClick={handleLocate}>
+        Locate
+      </button>
+      <SMap>
+        <MapContainer
+          center={[50.629, 3.057]}
+          zoom={13}
+          scrollWheelZoom
+          zoomControl={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+
+          <LocationMarker />
+        </MapContainer>
+      </SMap>
+    </>
   );
 }
