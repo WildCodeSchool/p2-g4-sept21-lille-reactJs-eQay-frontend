62c34e72a69ad48c4a737e6bfd9136ee048ed388
Pierre Goemans <p.goemans@hotmail.fr>
2022-01-05 14:44:44 +0100

    init context API

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import BurgerMenu from 'Components/BurgerMenu';
+import { useState } from 'react';
+import BurgerMenu from './Components/BurgerMenu';
+import EqaiContext from './Context/EqaiContext';
+
 import Map from './Components/Map';
 import 'reset-css';
 
 function App() {
+  const [findUser, setFindUser] = useState(false);
+
   return (
-    <>
+    <EqaiContext.Provider value={{ findUser, setFindUser }}>
       <BurgerMenu />
       <Map />
-    </>
+    </EqaiContext.Provider>
   );
 }
 
