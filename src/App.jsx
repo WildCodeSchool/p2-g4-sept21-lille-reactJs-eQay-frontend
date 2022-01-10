fb1f3678f3e6e30d8dc6ab4bc07b98173de2a0f0
Marion Grolleau <mariongrolleau@neuf.fr>
2022-01-10 12:25:45 +0100

    Add Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import HomePage from 'Components/HomePage';
+import News from 'Components/News';
+import Event from 'Components/Event';
+import GoodPractices from 'Components/GoodPractices';
+import AboutUs from 'Components/AboutUs';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/news" element={<News />} />
+      <Route path="/events" element={<Event />} />
+      <Route path="/goodpractices" element={<GoodPractices />} />
+      <Route path="/aboutus" element={<AboutUs />} />
     </Routes>
   );
 }
