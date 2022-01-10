3671cb079e1acf29a8aecf50367f65a84aaa8661
Marion Grolleau <mariongrolleau@neuf.fr>
2022-01-10 10:02:11 +0100

    Added global styled

--- src/style.jsx
+++ src/style.jsx
@@ -3,17 +3,11 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyle = createGlobalStyle`
     @import url('https://fonts.googleapis.com/css2?family=Dekko&display=swap');
 
-  body {
+  :root {
     --color-primary: #28abb9;
     --color-secondary: #2d6187;
     --color-third: #a8dda8;
     --font-family: 'Dekko', cursive;
-  
-  }
-
-  h1 {
-    color: var(--color-primary);
-    background-color: var(--color-third);
   }
 `;
 
