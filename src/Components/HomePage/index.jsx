7d6b5fa7bacbf3d3c9b5c0dcdb76402344ebeed8
Pierre Goemans <p.goemans@hotmail.fr>
2022-01-10 17:25:33 +0100

    add filter const in context

--- src/Components/HomePage/index.jsx
+++ src/Components/HomePage/index.jsx
@@ -7,9 +7,18 @@ import Map from 'Components/Map';
 export default function HomePage() {
   const [findUser, setFindUser] = useState(false);
   const [showExtSensor, setShowExtSensor] = useState(false);
+  const [filterValue, setFilterValue] = useState('');
+
   return (
     <EqaiContext.Provider
-      value={{ findUser, setFindUser, showExtSensor, setShowExtSensor }}
+      value={{
+        findUser,
+        setFindUser,
+        showExtSensor,
+        setShowExtSensor,
+        filterValue,
+        setFilterValue,
+      }}
     >
       <GlobalStyles />
       <BurgerMenu />
