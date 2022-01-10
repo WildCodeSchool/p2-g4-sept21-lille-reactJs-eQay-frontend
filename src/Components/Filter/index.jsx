eafa4c1bb5adf8f60641746ece4272b4820ced1c
Pierre Goemans <p.goemans@hotmail.fr>
2022-01-10 17:26:45 +0100

    Radio component for select value display

--- src/Components/Filter/index.jsx
+++ src/Components/Filter/index.jsx
@@ -12,11 +12,11 @@ export default function Filter() {
           setFilterValue(e.target.value);
         }}
       >
-        <input type="radio" value="Aqi" name="gender" /> Aqi
-        <input type="radio" value="Pm1" name="gender" /> Pm1
-        <input type="radio" value="Pm10" name="gender" /> Pm10
-        <input type="radio" value="Pm25" name="gender" /> Pm25
-        <input type="radio" value="Ppm" name="gender" /> Ppm
+        <input type="radio" value="aqi" name="gender" /> Aqi
+        <input type="radio" value="pm1" name="gender" /> Pm1
+        <input type="radio" value="pm10" name="gender" /> Pm10
+        <input type="radio" value="pm25" name="gender" /> Pm25
+        <input type="radio" value="ppm" name="gender" /> Ppm
         <div>{filterValue}</div>
       </div>
     </SRadio>
