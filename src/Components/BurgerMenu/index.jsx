320f8ac1051a2525207afc546b74b3fcd5b9d6ba
Marion Grolleau <mariongrolleau@neuf.fr>
2022-01-10 12:27:02 +0100

    Added Link in BurgerMenu

--- src/Components/BurgerMenu/index.jsx
+++ src/Components/BurgerMenu/index.jsx
@@ -1,19 +1,26 @@
 import { slide as Menu } from 'react-burger-menu';
+import { Link } from 'react-router-dom';
 import './style.css';
 
 export default function BurgerMenu() {
   return (
     <>
       <Menu>
-        <a id="home" className="menu-item" href="/">
-          Home
-        </a>
-        <a id="about" className="menu-item" href="/about">
-          About
-        </a>
-        <a id="contact" className="menu-item" href="/contact">
-          Contact
-        </a>
+        <Link id="home" className="menu-item" to="/">
+          Carte
+        </Link>
+        <Link id="news" className="menu-item" to="/news">
+          Infos
+        </Link>
+        <Link id="events" className="menu-item" to="/events">
+          Evénements
+        </Link>
+        <Link id="goodpractices" className="menu-item" to="/goodpractices">
+          Bonnes pratiques
+        </Link>
+        <Link id="about" className="menu-item" to="/aboutus">
+          Qui sommes-nous?
+        </Link>
       </Menu>
     </>
   );
