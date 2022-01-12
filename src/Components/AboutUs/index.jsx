423d858ae109d54af958b104913fc98aa1386542
Marion Grolleau <mariongrolleau@neuf.fr>
2022-01-12 10:41:22 +0100

    Added footer component in page AboutUs

--- src/Components/AboutUs/index.jsx
+++ src/Components/AboutUs/index.jsx
@@ -1,12 +1,14 @@
 import BurgerMenu from 'Components/BurgerMenu';
+import Footer from 'Components/Footer';
 import GlobalStyle from 'style';
 
 export default function AboutUs() {
   return (
     <>
+      <h1> Qui sommes nous ?</h1>
       <GlobalStyle />
       <BurgerMenu />
-      <h1> Qui sommes nous ?</h1>
+      <Footer />
     </>
   );
 }
