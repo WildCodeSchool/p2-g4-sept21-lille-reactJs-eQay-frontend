67bca6f3379ed65ebeb4bd9e239486ca89eba947
Marion Grolleau <mariongrolleau@neuf.fr>
2022-01-12 15:50:15 +0100

    Created Mailto component for footer

---
+++ src/Components/Mailto/style.jsx
@@ -0,0 +1,14 @@
+import Styled from 'styled-components';
+
+const SMailto = Styled.div`
+a {
+  text-decoration: none;
+  color: var(--color-secondary);
+}
+a:hover
+{
+  color: var(--color-primary)
+}
+`;
+
+export default SMailto;
