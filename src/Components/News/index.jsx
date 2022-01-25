7649ee3e78d3f591fb0a6d06346d9aae9f4e5cac
LuigiMenez <ltc.menez@gmail.com>
2022-01-25 15:27:38 +0100

    Add page Infos

--- src/Components/News/index.jsx
+++ src/Components/News/index.jsx
@@ -1,5 +1,5 @@
 import indoorPollution from 'assets/pollution-interieur-air-sain_6098113.jpg';
-import publicHealth from 'assets/sante-publique-france.png';
+// import publicHealth from 'assets/sante-publique-france.png';
 import didYouKnow from 'assets/didYouKnow.png';
 import erp from 'assets/erp.png';
 import moreInformations from 'assets/info.png';
@@ -23,27 +23,28 @@ export default function News() {
               Oui, notre santé commence par l&#39;air que nous respirons !
             </h3>
           </div>
+
           <div className="imgContent">
-            <p className="contentNews">
+            <p className="contentNews" img={indoorPollution}>
               En moyenne nous passons quotidiennement 80% de notre temps à
               l&#39;intérieur de bâtiments dans lesquels l&#39;air que nous
               respirons peut être jusqu&#39;à 10x plus pollué qu&#39;à
               l&#39;extérieur.
             </p>
-            <img
-              className="imgIndoorPollution"
-              src={indoorPollution}
-              alt="pollution intérieur"
-            />
           </div>
+          <img
+            className="imgIndoorPollution"
+            src={indoorPollution}
+            alt="pollution intérieur"
+          />
         </article>
         <section>
           <div className="headRegulation">
-            <img
+            {/* <img
               className="imgPublicHealth"
               src={publicHealth}
               alt="santé publique france"
-            />
+            /> */}
             <div className="titleRegulation">
               <h2>La Réglementation QAI en France</h2>
               <p>
@@ -54,10 +55,10 @@ export default function News() {
             </div>
           </div>
         </section>
-        <section>
+        <section className="regulation">
           <h3>Quels sont les ERP soumis à cette réglementation ?</h3>
           <p>Cette obligation s&#39;applique notamment aux :</p>
-          <ul className>
+          <ul>
             <li>
               Établissements d&#39;accueil collectif d&#39;enfants de moins de
               six ans (crèches, halte-garderie, jardins d&#39;enfants, etc.) ;
@@ -76,7 +77,7 @@ export default function News() {
               312-1 du code de l&#39;action sociale et des familles).
             </li>
           </ul>
-          <p>
+          <p className="toKnow">
             <span>A noter:</span> cette réglementation vise aussi bien les
             établissements publics, gérés par les collectivités (mairies,
             communautés de commune ou d&#39;agglomération, département, région,
@@ -91,7 +92,7 @@ export default function News() {
             gestionnaire de l&#39;établissement sera la personne de référence
             pour le suivi de ces actions.
           </p>
-          <p className>Une entrée en vigueur progressive</p>
+          <h3>Une entrée en vigueur progressive</h3>
           <p>
             L&#39;entrée en vigueur de ce nouveau dispositif est progressive et
             la surveillance devra être achevée avant le :
@@ -110,19 +111,16 @@ export default function News() {
             <li>1er janvier 2023 pour les autres établissements visés.</li>
           </ul>
           <figure>
-            <div>
+            <div className="moreInformation">
               <img src={moreInformations} alt="Plus d'information" />
-            </div>
-            <div>
-              <a
-                href=" https://jurad-bat.net/collectivites/reglementation-QAI-france"
-                className="small-marginBottom"
-              >
-                https://jurad-bat.net/collectivites/reglementation-QAI-france
-              </a>
-              <a href="https://www.ecologie.gouv.fr/qualite-lair-interieur">
-                https://www.ecologie.gouv.fr/qualite-lair-interieur
-              </a>
+              <div className="link">
+                <a href=" https://jurad-bat.net/collectivites/reglementation-QAI-france">
+                  https://jurad-bat.net/collectivites/reglementation-QAI-france
+                </a>
+                <a href="https://www.ecologie.gouv.fr/qualite-lair-interieur">
+                  https://www.ecologie.gouv.fr/qualite-lair-interieur
+                </a>
+              </div>
             </div>
           </figure>
           <p>
--- src/Components/News/style.jsx
+++ src/Components/News/style.jsx
@@ -6,13 +6,13 @@ const SNews = styled.article`
     clip-path: polygon(0 0, 100vw 0, 100vw 9vh, 0 20vh);
     background-image: linear-gradient(
       to right bottom,
-      rgba(126, 213, 111, 0.8),
-      rgba(40, 180, 133, 0.8)
+      rgba(126, 213, 111),
+      rgba(40, 180, 133)
     );
     background-image: linear-gradient(
       to right bottom,
-      rgba($color-primary-light, 0.8),
-      rgba($color-primary-dark, 0.8)
+      rgba($color-primary-light),
+      rgba($color-primary-dark)
     );
     height: 20vh;
     text-align: center;
@@ -27,56 +27,97 @@ const SNews = styled.article`
     text-align: center;
     font-size: 25px;
   }
-
   .title {
     display: flex;
-    padding-top: 150px;
+    padding-top: 23vh;
+    justify-content: center;
     align-items: center;
   }
   .imgDidYouKnow {
     height: 25vh;
   }
   .imgContent {
+    background-color: red;
     position: relative;
-    width: 100%;
+    width: 100vw;
     height: 70%;
-    text-align: center;
+    margin: auto;
   }
   .contentNews {
+    background: url(${(props) => props.img});
     position: absolute;
-    width: 55%;
+    width: 30%;
     font-size: 22px;
     text-align: center;
-    margin: 10vh;
     font-weight: bold;
+    margin: auto;
+    margin: red solid 2px;
   }
   .imgIndoorPollution {
+    display: flex;
+    border: solid 2px blue;
     z-index: 1;
     box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
     border-radius: 5px;
+    width: 60%;
   }
   .imgPublicHealth {
     width: 15%;
     height: 20%;
   }
-
   .headRegulation {
     display: flex;
     margin: 50px;
     justify-content: center;
-  
-
+  }
   .titleRegulation {
     display: flex;
     flex-direction: column;
     justify-content: center;
     text-align: center;
-    }
-
+  }
   .organigramme {
-    border: 1px solid red;
     width: 90vw;
-     }
+  }
+  h2 {
+    padding: 4vh;
+    text-align: center;
+    font-size: 25px;
+  }
+  p {
+    font-size: 18px;
+    padding: 10px;
+  }
+  .regulation {
+    width: 70vw;
+    margin: auto;
+  }
+  span {
+    font-weight: bold;
+  }
+  ul {
+    list-style-type: disc;
+    padding: 0 0 0 40px;
+  }
+  li {
+    padding: 5px;
+  }
+  .toKnow {
+    background: #9e9e9e6e;
+    margin-top: 15px;
+  }
+  .link {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+  .moreInformation {
+    display: flex;
+    width: 70vw;
+  }
+  a {
+    padding-bottom: 15px;
+  }
 `;
 
 export default SNews;
