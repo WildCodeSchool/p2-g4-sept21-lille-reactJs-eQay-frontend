94586cbca131d35d6c52e0a3fae37532a296040e
LoicBrassart <loic.brassart@gmail.com>
2022-01-27 16:51:06 +0100

    Try to fix performances for MarkerIndoor

--- src/Components/MarkerIndoor/index.jsx
+++ src/Components/MarkerIndoor/index.jsx
@@ -3,7 +3,6 @@ import { useState, useEffect, useContext } from 'react';
 import L from 'leaflet';
 import greenAir from 'assets/greenAir.png';
 import yellowAir from 'assets/yellowAir.png';
-import orangeAir from 'assets/orangeAir.png';
 import redAir from 'assets/redAir.png';
 import greyAir from 'assets/greyAir.png';
 import { Marker, Popup } from 'react-leaflet';
@@ -23,11 +22,6 @@ const yellowAirIcon = new L.Icon({
   iconSize: [40, 40],
   iconAnchor: [20, 40],
 });
-const orangeAirIcon = new L.Icon({
-  iconUrl: orangeAir,
-  iconSize: [40, 40],
-  iconAnchor: [20, 40],
-});
 const redAirIcon = new L.Icon({
   iconUrl: redAir,
   iconSize: [40, 40],
@@ -40,7 +34,9 @@ const greyAirIcon = new L.Icon({
 });
 export default function MarkerIndoor() {
   const [allDataIndoors, setAllDataIndoors] = useState([]);
+  const [markers, setMarkers] = useState(<></>);
   const { filterValue } = useContext(EqaiContext);
+
   function changeToLocalDate(date) {
     const parseDate = Date.parse(date);
     const newdate = new Date(parseDate);
@@ -55,84 +51,112 @@ export default function MarkerIndoor() {
     });
     return localDate.replace(',', ' ');
   }
+
   useEffect(() => {
     try {
       axios.get('http://localhost:5050/indoor').then(({ data }) => {
-        setAllDataIndoors(data);
+        const finalData = data.filter((value) => value[0].type === 'Int');
+        setAllDataIndoors(finalData);
       });
     } catch (error) {
       console.log(error);
     }
   }, []);
-  function switchIcon(value) {
-    if (value <= 0) {
-      return greyAirIcon;
-    }
-    if (filterValue === 'aqi' && value > 50) {
-      return greenAirIcon;
-    }
-    if (filterValue === 'aqi' && value < 50) {
-      return redAirIcon;
-    }
-    if (filterValue === 'pm1' && value < 10) {
-      return greenAirIcon;
-    }
-    if (filterValue === 'pm1' && value > 10 && value <= 20) {
-      return yellowAirIcon;
-    }
-    if (filterValue === 'pm1' && value > 20) {
-      return redAirIcon;
-    }
-    if (filterValue === 'pm10' && value < 20) {
-      return greenAirIcon;
-    }
-    if (filterValue === 'pm10' && value > 20 && value <= 40) {
-      return yellowAirIcon;
-    }
-    if (filterValue === 'pm10' && value > 40) {
-      return redAirIcon;
-    }
-    if (filterValue === 'pm25' && value < 15) {
-      return greenAirIcon;
-    }
-    if (filterValue === 'pm25' && value > 15 && value <= 25) {
-      return yellowAirIcon;
-    }
-    if (filterValue === 'pm25' && value > 25) {
-      return redAirIcon;
-    }
-    if (filterValue === 'ppm' && value < 50) {
-      return greenAirIcon;
-    }
-    if (filterValue === 'ppm' && value > 50) {
-      return orangeAirIcon;
-    }
-    if (filterValue === 'temperature' && value < 18 && value > 24) {
-      return greenAirIcon;
-    }
-    if (
-      (filterValue === 'temperature' && value < 14 && value >= 18) ||
-      (value > 24 && value < 28)
-    ) {
-      return yellowAirIcon;
-    }
-    if ((filterValue === 'temperature' && value < 14) || value >= 28) {
-      return yellowAirIcon;
-    }
-    if (filterValue === 'humidity' && value >= 30 && value < 60) {
-      return greenAirIcon;
-    }
-    if (
-      (filterValue === 'humidity' && value >= 20 && value < 30) ||
-      (value >= 60 && value < 80)
-    ) {
-      return yellowAirIcon;
-    }
-    if ((filterValue === 'humidity' && value < 20) || value >= 80) {
-      return redAirIcon;
-    }
-    return yellowAirIcon;
-  }
+
+  useEffect(() => {
+    const tmpMarkers = allDataIndoors.map((e) => {
+      const value = e[0][filterValue];
+      let icon = greyAirIcon;
+      let filter = 'N/A';
+      let unit;
+      let displayValue = 0;
+      switch (filterValue) {
+        case 'aqi':
+          filter = 'Aqi';
+          unit = '';
+          displayValue = parseInt(e[0].aqi, 10);
+          if (value > 50) icon = greenAirIcon;
+          else icon = redAirIcon;
+          break;
+        case 'pm1':
+          filter = 'Pm1';
+          unit = 'µg/m3';
+          displayValue = parseInt(e[0].pm1, 10);
+          if (value <= 10) icon = greenAirIcon;
+          else if (value > 10 && value <= 20) icon = yellowAirIcon;
+          else icon = redAirIcon;
+          break;
+        case 'pm10':
+          filter = 'Pm10';
+          unit = 'µg/m3';
+          displayValue = parseInt(e[0].pm10, 10);
+          if (value <= 20) icon = greenAirIcon;
+          else if (value > 20 && value <= 40) icon = yellowAirIcon;
+          else icon = redAirIcon;
+          break;
+        case 'pm25':
+          filter = 'Pm25';
+          unit = 'µg/m3';
+          displayValue = parseInt(e[0].pm25, 10);
+          if (value <= 15) icon = greenAirIcon;
+          else if (value > 15 && value <= 25) icon = yellowAirIcon;
+          else icon = redAirIcon;
+          break;
+        case 'ppm':
+          filter = 'Ppm';
+          unit = 'mg/kg';
+          displayValue = parseInt(e[0].ppm, 10);
+          if (value < 50) icon = greenAirIcon;
+          else icon = redAirIcon;
+          break;
+        case 'temperature':
+          filter = 'Température';
+          unit = '°C';
+          displayValue = parseInt(e[0].temperature, 10);
+          if (value <= 14 || value > 28) icon = redAirIcon;
+          else if (value <= 18 || value > 24) icon = yellowAirIcon;
+          else icon = greenAirIcon;
+          break;
+        case 'humidity':
+          filter = 'Humidité';
+          unit = '%';
+          displayValue = parseInt(e[0].humidity, 10);
+          if (value <= 20 || value > 80) icon = redAirIcon;
+          else if (value <= 30 || value > 60) icon = yellowAirIcon;
+          else icon = greenAirIcon;
+          break;
+        default:
+          icon = greyAirIcon;
+          break;
+      }
+      if (value <= 0) {
+        icon = greyAirIcon;
+      }
+
+      return (
+        <Marker
+          position={[e[0].adresses_latitude, e[0].adresses_longitude]}
+          icon={icon}
+          key={e[0].id}
+        >
+          <Popup>
+            <SPopup>
+              <ul>
+                <li>{`${filter} : ${displayValue} ${unit}`}</li>
+                <li className="date">
+                  Date : {changeToLocalDate(e[0].timestamp)}
+                </li>
+              </ul>
+              <h2>Partager</h2>
+              <Share data={e[0]} />
+            </SPopup>
+          </Popup>
+        </Marker>
+      );
+    });
+    setMarkers(tmpMarkers);
+  }, [filterValue]);
+
   return (
     <MarkerIndoorGroup
       spiderfyDistanceMultiplier={1}
@@ -144,48 +168,7 @@ export default function MarkerIndoor() {
         });
       }}
     >
-      {allDataIndoors.map((e) => {
-        return e[0].type === 'Int' ? (
-          <Marker
-            position={[e[0].adresses_latitude, e[0].adresses_longitude]}
-            icon={switchIcon(e[0][filterValue])}
-            key={e[0].id}
-          >
-            <Popup>
-              <SPopup>
-                <ul>
-                  {filterValue === 'aqi' ? (
-                    <li>Aqi : {parseInt(e[0].aqi, 10)}</li>
-                  ) : null}
-                  {filterValue === 'pm1' ? (
-                    <li>Pm1 : {parseInt(e[0].pm1, 10)} µg/m3</li>
-                  ) : null}
-                  {filterValue === 'pm10' ? (
-                    <li>Pm10 : {parseInt(e[0].pm10, 10)} µg/m3</li>
-                  ) : null}
-                  {filterValue === 'pm25' ? (
-                    <li>Pm2,5 : {parseInt(e[0].pm25, 10)} µg/m3</li>
-                  ) : null}
-                  {filterValue === 'ppm' ? (
-                    <li>Ppm : {parseInt(e[0].ppm, 10)} mg/kg</li>
-                  ) : null}
-                  {filterValue === 'temperature' ? (
-                    <li>Température : {parseInt(e[0].temperature, 10)} °c</li>
-                  ) : null}
-                  <li className="date">
-                    Date : {changeToLocalDate(e[0].timestamp)}
-                  </li>
-                  {filterValue === 'humidity' ? (
-                    <li>Humidité : {e[0].humidity} %</li>
-                  ) : null}
-                </ul>
-                <h2>Partager</h2>
-                <Share data={e[0]} />
-              </SPopup>
-            </Popup>
-          </Marker>
-        ) : null;
-      })}
+      {markers}
     </MarkerIndoorGroup>
   );
 }
