7d231cf6c6769517235ffe3e5f18eee079d372c9
fortanza <undertaker040880@gmail.com>
2022-02-09 14:23:12 +0100

    Add link to eqai Logo

--- src/Components/eqaiLogo/index.jsx
+++ src/Components/eqaiLogo/index.jsx
@@ -1,11 +1,14 @@
 import eqai from 'assets/eqai.png';
+import { Link } from 'react-router-dom';
 import './style.css';
 
 export default function EqaiLogo() {
   return (
     <>
       <div className="logoContainer">
-        <img src={eqai} alt="eqaiLogo" className="logo" />
+        <Link id="home" className="menuItem" to="/">
+          <img src={eqai} alt="eqaiLogo" className="logo" />
+        </Link>
       </div>
     </>
   );
